40ad7efbb240e8911bc7c325318e4f38e534dcef
kevinkotcherga <kevin.kotcherga@gmail.com>
2021-07-02 16:13:25 +0200

    ez

--- projet2/indexprojet2.php
+++ projet2/indexprojet2.php
@@ -13,10 +13,10 @@
 
       <div class="scholarme-page">
         <header>
-          <img src="../images/image-projet2/logo.png">
+          <img class="logo-uni" src="../images/image-projet2/logo.png">
         </header>
 
-        <section>
+        <section class="section-principale">
           <h1>La carte pour payer en tois fois sans frais</h1>
 
             <div>
