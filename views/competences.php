4d84f6754c015aef90743557210ed421033db864
kevinkotcherga <kevin.kotcherga@gmail.com>
2021-08-24 12:45:35 +0200

    Rebase clean

--- views/competences.php
+++ views/competences.php
@@ -12,19 +12,27 @@
     <section>
       <div class="container">
 
-      <?php include("../php/header.php"); ?>
+        <div class="imgDoodle3">
+            <img src="../pictures/custom/object3.png">
+          </div>
 
-              <div class="section-competences">
-                <p><img class="html5" src="../pictures/html-black.png" alt="">Html 5</p>
-                <p><img class="bootstrap"  src="../pictures/bootstrap.png" alt="">Bootstrap</p>
-                <p><img class="css3" src="../pictures/css3-black.png" alt="">Css 3</p>
-                <p><img class="php" src="../pictures/php.png" alt="">Php</p>
-                <p><img class="visual" src="../pictures/visual_studio.png" alt="">Visual Studio Code</p>
-                <p><img class="javascript" src="../pictures/javascript.png" alt="">JavaScript</p>
-                <p><img class="sass" src="../pictures/sass-white.png" alt="">Sass & BEM</p>
-                <p><img class="responsive" src="../pictures/multiscreen.png" alt="">Responsive Design</p>
-                <p><img class="git" src="../pictures/Git-Logo-Black.png" alt="">Git & GitHub</p>  
-              </div>  
+          <div class="imgDoodle4">
+            <img src="../pictures/custom/object3.png">
+          </div>
+
+        <?php include("../php/header.php"); ?>
+
+                <div class="section-competences">
+                  <p><img class="html5" src="../pictures/html-black.png" alt="">Html 5</p>
+                  <p><img class="bootstrap"  src="../pictures/bootstrap.png" alt="">Bootstrap</p>
+                  <p><img class="css3" src="../pictures/css3-black.png" alt="">Css 3</p>
+                  <p><img class="php" src="../pictures/php.png" alt="">Php</p>
+                  <p><img class="visual" src="../pictures/visual_studio.png" alt="">Visual Studio Code</p>
+                  <p><img class="javascript" src="../pictures/javascript.png" alt="">JavaScript</p>
+                  <p><img class="sass" src="../pictures/sass-white.png" alt="">Sass & BEM</p>
+                  <p><img class="responsive" src="../pictures/multiscreen.png" alt="">Responsive Design</p>
+                  <p><img class="git" src="../pictures/Git-Logo-Black.png" alt="">Git & GitHub</p>  
+                </div>  
 
         <?php include("../php/footer.php"); ?>
 
